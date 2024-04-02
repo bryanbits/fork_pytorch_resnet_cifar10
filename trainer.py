84e1b96f3a473f586ce1173e69164d5876b90a6b
bryanbits <bryanliu18@gmaillcom>
2024-04-01 18:19:41 -0700

    fix checkpoint finetune loading

--- trainer.py
+++ trainer.py
@@ -96,11 +96,12 @@ def main():
         if os.path.isfile(args.resume):
             print("=> loading checkpoint '{}'".format(args.resume))
             checkpoint = torch.load(args.resume)
-            args.start_epoch = checkpoint['epoch']
-            best_prec1 = checkpoint['best_prec1']
+            if not args.finetune:
+                args.start_epoch = checkpoint['epoch']
+                best_prec1 = checkpoint['best_prec1']
             model.load_state_dict(checkpoint['state_dict'])
             print("=> loaded checkpoint '{}' (epoch {})"
-                  .format(args.evaluate, checkpoint['epoch']))
+                  .format(args.evaluate, args.start_epoch))
         else:
             print("=> no checkpoint found at '{}'".format(args.resume))
 
